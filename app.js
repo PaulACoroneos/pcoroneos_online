825fdd7f077616f66f701c3fa8133106aedaf4ba
Paul Coroneos <paul.coroneos@icloud.com>
2024-08-17 12:53:06 +0000

    initial files

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+http.createServer(function (req,res) {
+	res.write("On the the way to being a full stack engineer");
+	res.end();
+}).listen(3000);
+
+console.log("Server started on PORT 3000");
+
